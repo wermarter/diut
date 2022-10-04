24ddecf22d2174eab2c523c3dac205f568b7603c
Ha Minh Chien <wermarter@gmail.com>
2022-10-04 10:41:19 +0700

    add 404 page

--- apps/web-app/src/common/layout/ErrorPage/index.tsx
+++ apps/web-app/src/common/layout/ErrorPage/index.tsx
@@ -1,17 +1,23 @@
-import { Navigate, useRouteError } from 'react-router-dom'
+import { isRouteErrorResponse, Navigate, useRouteError } from 'react-router-dom'
 
 import { AppException, AppNavigate } from 'src/common/utils'
 import { AppExceptionPage } from './app-exception'
+import { NotFoundPage } from './not-found'
 import { UnknownExceptionPage } from './unknown-exception'
 import { WeirdExceptionPage } from './weird-error'
 
 export function ErrorPage() {
   const error = useRouteError()
 
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    return <NotFoundPage />
+  }
+
   if (error instanceof AppNavigate) {
     return <Navigate {...error.props} />
   }
 
+  // ------------------------------------------------------------------------------
   if (!(error instanceof Error)) {
     return <WeirdExceptionPage />
   }
---
+++ apps/web-app/src/common/layout/ErrorPage/not-found.tsx
@@ -0,0 +1,11 @@
+import { Alert } from '@mui/material'
+
+import { ErrorLayout } from './error-layout'
+
+export function NotFoundPage() {
+  return (
+    <ErrorLayout>
+      <Alert severity="error">Không tìm thấy liên kết này</Alert>
+    </ErrorLayout>
+  )
+}
