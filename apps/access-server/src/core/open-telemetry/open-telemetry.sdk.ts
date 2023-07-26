11bb8b1c86137dc75f4534a11ea42dfc1c6a9bb1
Ha Minh Chien <wermarter@gmail.com>
2023-07-26 17:43:47 +0700

    refactor

--- apps/access-server/src/core/open-telemetry/open-telemetry.sdk.ts
+++ apps/access-server/src/core/open-telemetry/open-telemetry.sdk.ts
@@ -4,69 +4,50 @@ import {
   W3CTraceContextPropagator,
   W3CBaggagePropagator,
 } from '@opentelemetry/core'
-import {
-  BatchSpanProcessor,
-  SimpleSpanProcessor,
-} from '@opentelemetry/sdk-trace-base'
-import { JaegerExporter } from '@opentelemetry/exporter-jaeger'
-import { JaegerPropagator } from '@opentelemetry/propagator-jaeger'
-import { B3InjectEncoding, B3Propagator } from '@opentelemetry/propagator-b3'
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus'
 import { NodeSDK } from '@opentelemetry/sdk-node'
 import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 import { Resource } from '@opentelemetry/resources'
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
-import { MongooseInstrumentation } from '@opentelemetry/instrumentation-mongoose'
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 
-const traceExporter = new JaegerExporter({})
-
-const spanProcessor =
-  process.env.NODE_ENV === `development`
-    ? new SimpleSpanProcessor(traceExporter)
-    : new BatchSpanProcessor(traceExporter)
+const traceExporter = new OTLPTraceExporter({
+  url: 'http://tempo:4318/v1/traces',
+})
 
 const otelSDK = new NodeSDK({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: `access-server`, // update this to a more relevant name for you!
+    [SemanticResourceAttributes.SERVICE_NAME]: process.env.SERVICE_NAME, // update this to a more relevant name for you!
   }),
-
   metricReader: new PrometheusExporter({
     port: 8081,
+    endpoint: '/metrics',
   }),
-  spanProcessor,
+  spanProcessor: new BatchSpanProcessor(traceExporter),
   contextManager: new AsyncLocalStorageContextManager(),
   textMapPropagator: new CompositePropagator({
-    propagators: [
-      new JaegerPropagator(),
-      new W3CTraceContextPropagator(),
-      new W3CBaggagePropagator(),
-      new B3Propagator(),
-      new B3Propagator({
-        injectEncoding: B3InjectEncoding.MULTI_HEADER,
-      }),
-    ],
+    propagators: [new W3CTraceContextPropagator(), new W3CBaggagePropagator()],
   }),
   instrumentations: [
-    // getNodeAutoInstrumentations({
-    //   '@opentelemetry/instrumentation-mongodb': {
-    //     enabled: false,
-    //   },
-    //   '@opentelemetry/instrumentation-fs': {
-    //     enabled: false,
-    //   },
-    //   '@opentelemetry/instrumentation-dns': {
-    //     enabled: false,
-    //   },
-    //   '@opentelemetry/instrumentation-express': {
-    //     enabled: false,
-    //   },
-    //   '@opentelemetry/instrumentation-net': {
-    //     enabled: false,
-    //   },
-    // }),
-    new HttpInstrumentation(),
-    new MongooseInstrumentation(),
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-mongodb': {
+        enabled: false,
+      },
+      '@opentelemetry/instrumentation-fs': {
+        enabled: false,
+      },
+      '@opentelemetry/instrumentation-dns': {
+        enabled: false,
+      },
+      '@opentelemetry/instrumentation-express': {
+        enabled: false,
+      },
+      '@opentelemetry/instrumentation-net': {
+        enabled: false,
+      },
+    }),
   ],
 })
 
