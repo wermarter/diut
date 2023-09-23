64ee74f2c03aa5493b5e948cab2902ee65438509
Ha Minh Chien <wermarter@gmail.com>
2023-09-23 09:02:26 +0700

    remove onchange

--- apps/bathanghai-server/src/configs/log.config.ts
+++ apps/bathanghai-server/src/configs/log.config.ts
@@ -1,11 +1,12 @@
 // import { registerAs } from '@nestjs/config'
 import { makeConfigLoader } from '@diut/server-core'
-import { IsNotEmpty, IsString } from 'class-validator'
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator'
 
 export class LogConfig {
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
-  LOKI_URL: string
+  LOKI_URL?: string
 }
 
 export const loadLogConfig = makeConfigLoader(LogConfig)
