e4fd397d34df24b04548c0c29961db1c1d9ec1bf
Ha Minh Chien <wermarter@gmail.com>
2023-11-10 16:20:07 +0700

    udpate dependencies

--- libs/server-core/src/otel/index.ts
+++ libs/server-core/src/otel/index.ts
@@ -1,2 +1,3 @@
 export * from './otel.bootstrap'
 export * from './otel.module'
+export * from './otel.config'
--- libs/server-core/src/otel/otel.bootstrap.ts
+++ libs/server-core/src/otel/otel.bootstrap.ts
@@ -1,5 +1,5 @@
 import { NodeSDK } from '@opentelemetry/sdk-node'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+// import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { Resource } from '@opentelemetry/resources'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus'
@@ -9,6 +9,8 @@ import {
   W3CBaggagePropagator,
   W3CTraceContextPropagator,
 } from '@opentelemetry/core'
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base'
+// import { trace } from '@opentelemetry/api'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 
 import { BootstrapConfig } from '../bootstrap'
@@ -22,9 +24,9 @@ export const OtelBootstrap: BootstrapConfig = {
     const { PROMETHEUS_ENDPOINT, PROMETHEUS_PORT, OTLP_TRACE_EXPORTER_URL } =
       loadConfigFromEnv(OtelConfig)
 
-    const traceExporter = new OTLPTraceExporter({
-      url: OTLP_TRACE_EXPORTER_URL,
-    })
+    // const traceExporter = new OTLPTraceExporter({
+    //   url: OTLP_TRACE_EXPORTER_URL,
+    // })
     const metricExporter = new PrometheusExporter({
       port: PROMETHEUS_PORT,
       endpoint: PROMETHEUS_ENDPOINT,
@@ -35,7 +37,7 @@ export const OtelBootstrap: BootstrapConfig = {
         [SemanticResourceAttributes.SERVICE_NAME]: ctx.serviceName,
       }),
       metricReader: metricExporter,
-      traceExporter,
+      traceExporter: new ConsoleSpanExporter(),
       contextManager: new AsyncLocalStorageContextManager(),
       textMapPropagator: new CompositePropagator({
         propagators: [
@@ -45,26 +47,39 @@ export const OtelBootstrap: BootstrapConfig = {
       }),
       instrumentations: [
         getNodeAutoInstrumentations({
-          '@opentelemetry/instrumentation-mongodb': {
-            enabled: false,
-          },
+          // '@opentelemetry/instrumentation-mongodb': {
+          //   enabled: false,
+          // },
           '@opentelemetry/instrumentation-fs': {
             enabled: false,
           },
-          '@opentelemetry/instrumentation-dns': {
-            enabled: false,
-          },
-          '@opentelemetry/instrumentation-express': {
-            enabled: false,
-          },
-          '@opentelemetry/instrumentation-net': {
-            enabled: false,
-          },
+          // '@opentelemetry/instrumentation-dns': {
+          //   enabled: false,
+          // },
+          // '@opentelemetry/instrumentation-express': {
+          //   enabled: false,
+          // },
+          // '@opentelemetry/instrumentation-net': {
+          //   enabled: false,
+          // },
         }),
       ],
     })
 
     OtelSDK.start()
+
+    // trace
+    //   .getTracerProvider()
+    //   .getTracer('debug')
+    //   .startSpan('test manual from main app')
+    //   .end()
+    // setInterval(() => {
+    //   trace
+    //     .getTracerProvider()
+    //     .getTracer('debug')
+    //     .startSpan('test manual span each 10 second')
+    //     .end()
+    // }, 10000)
   },
   onExit() {
     OtelSDK.shutdown()
