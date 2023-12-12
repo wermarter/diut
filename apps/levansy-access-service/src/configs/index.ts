a5cd83ec6b0ae32d1fb4c4e20b25eee0e01068ad
Ha Minh Chien <wermarter@gmail.com>
2023-12-12 17:53:22 +0700

    remove

--- apps/levansy-access-service/src/configs/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './app.config'
-export * from './auth.config'
-export * from './log.config'
-export * from './minio.config'
-export * from './mongo.config'
-export * from './client.config'
