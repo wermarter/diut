5672271c617edfd488bf1329a441dfaa0ef750ae
Ha Minh Chien <wermarter@gmail.com>
2023-12-18 17:10:40 +0700

    update

--- libs/nest-core/src/mongo/mongo.common.ts
+++ libs/nest-core/src/mongo/mongo.common.ts
@@ -1,7 +1,12 @@
 import { SchemaOptions } from '@nestjs/mongoose'
-import { Expose } from 'class-transformer'
+import { Expose, Transform } from 'class-transformer'
 import { ApiProperty } from '@nestjs/swagger'
-import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common'
+import {
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+  applyDecorators,
+} from '@nestjs/common'
 import { ObjectId, Types } from 'mongoose'
 import {
   registerDecorator,
@@ -19,7 +24,20 @@ export const exampleDate = {
   example: '2018-03-20T09:12:28Z',
 }
 
+// https://github.com/typestack/class-transformer/issues/494
+export const ExposeObjectId = () =>
+  applyDecorators(
+    Expose(),
+    Transform(
+      ({ obj, key }) => {
+        return obj[key]
+      },
+      { toClassOnly: true },
+    ),
+  )
+
 export class BaseResourceResponseDto {
+  @ExposeObjectId()
   @ApiProperty(exampleMongoObjectId)
   _id: string
 
