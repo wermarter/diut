974c3fdefed75e6fc394f05cada02cd577f4b486
Ha Minh Chien <wermarter@gmail.com>
2024-04-17 22:07:14 +0700

    update

--- k8s/dependencies/3-longhorn/argo-down.sh
+++ k8s/dependencies/3-longhorn/argo-down.sh
@@ -3,4 +3,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 kubectl=/snap/bin/kubectl
 
+$kubectl -n longhorn-system patch -p '{"value": "true"}' --type=merge lhs deleting-confirmation-flag
 $kubectl delete -f $SCRIPT_DIR/argocd-application.yaml
