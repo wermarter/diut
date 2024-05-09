33a65f41d3e3c8e40f6cd38400cf1eb97a225ee8
Ha Minh Chien <wermarter@gmail.com>
2024-05-09 14:21:48 +0700

    update

---
+++ k8s/dependencies/10-kafka/helm-up.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+kubectl=/snap/bin/kubectl
+
+helm upgrade --install -n diut kafka $SCRIPT_DIR --render-subchart-notes
