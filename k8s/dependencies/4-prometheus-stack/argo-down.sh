c7a752408599924af532748fc61e54ce5aec84a6
Ha Minh Chien <wermarter@gmail.com>
2024-07-02 17:31:16 +0700

    optimize deployment

--- k8s/dependencies/4-prometheus-stack/argo-down.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-kubectl=/snap/bin/kubectl
-
-$kubectl delete -f $SCRIPT_DIR/argocd-application.yaml
--- k8s/dependencies/4-prometheus-stack/argo-up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-kubectl=/snap/bin/kubectl
-
-$kubectl apply -f $SCRIPT_DIR/argocd-application.yaml
