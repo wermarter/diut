6342450eb6f1a33b0deff7b8ccd6672c866da04e
Ha Minh Chien <wermarter@gmail.com>
2024-09-06 10:08:41 +0700

    optimize loki label

--- k8s/dependencies/11-otel/argo-down.sh
+++ k8s/dependencies/11-otel/argo-down.sh
@@ -3,5 +3,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 kubectl=/snap/bin/kubectl
 
-$kubectl delete -f $SCRIPT_DIR/src/lowa.yaml
 $kubectl delete -f $SCRIPT_DIR/argocd-application.yaml
