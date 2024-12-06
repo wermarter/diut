1a005b9645233aad2f4fe182c9194552982a61c9
Ha Minh Chien <wermarter@gmail.com>
2024-12-06 21:11:23 +0700

    update

--- apps/hcdc-web-app/src/features/homepage/pages/Homepage/index.tsx
+++ apps/hcdc-web-app/src/features/homepage/pages/Homepage/index.tsx
@@ -1,10 +1,9 @@
 export function HomePage() {
   return (
-    // <iframe
-    //   src="https://hcdc.vn/"
-    //   style={{ width: '100%', height: '100%' }}
-    //   frameBorder="0"
-    // ></iframe>
-    <h1></h1>
+    <iframe
+      src="https://hcdc.vn/"
+      style={{ width: '100%', height: '100%' }}
+      frameBorder="0"
+    ></iframe>
   )
 }
